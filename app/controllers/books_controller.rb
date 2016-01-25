7f07c68b2bac68c673a64e7a2dcd39b6bef56b92
prabhdubey <prabh131339@gmail.com>
2016-01-25 00:57:48 +0000

    Edit Dropdown

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,68 +1,66 @@
 class BooksController < ApplicationController
-	
-	before_action :find_book, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, only: [:edit, :new]
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
 
-	def index
+    def index
         if params[:category].blank?
-		   @books=Book.all.order("created_at DESC")
+            @books = Book.all.order("created_at DESC")
         else
-            @category_id=Category.find_by(name: params[:category]).id
-            @books= Book.where(:category_id => @category_id).order("created_at DESC")
+            @category_id = Category.find_by(name: params[:category]).id
+            @books = Book.where(:category_id => @category_id).order("created_at DESC")
         end
-	end
+    end
 
-     def new 
-       	@book=current_user.books.build
-        @categories= Category.all.map{ |c| [c.name,c.id] }
-     end
-
-     def show
+    def show
         if @book.reviews.blank?
-            @average_rating =0
+            @average_review = 0
         else
-            @average_rating = @book.reviews.average(:rating).round(2)
+            @average_review = @book.reviews.average(:rating).round(2)
         end
-     end
-     
+    end
+
+    def new
+        @book = current_user.books.build
+        @categories = Category.all.map{ |c| [c.name, c.id] }
+    end
+
+    def create
+        @book = current_user.books.build(book_params)
+        @book.category_id = params[:category_id]
+
+        if @book.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
     def edit
-         @categories= Category.all.map{ |c| [c.name,c.id] }
+        @categories = Category.all.map{ |c| [c.name, c.id] }
     end
 
     def update
-         @book.category_id= params[:category_id]
-    	
-        if @book.update(books_params)
-    		redirect_to book_path(@book)
-    	else
-    		render 'edit'
-    	end
-    
+        @book.category_id = params[:category_id]
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+            render 'edit'
+        end
     end
 
     def destroy
         @book.destroy
-    	redirect_to root_path
+        redirect_to root_path
     end
 
-     def create
-     	@book=current_user.books.build(books_params)
-        @book.category_id= params[:category_id]
-     	if @book.save  #whether book is saved or not
-     		redirect_to root_path  #move to index page
-     	else
-     		render 'new'   #else render new form
-     	end
-  	 end
+    private
 
-private
-	
-	def books_params
-		params.require(:book).permit(:title, :description, :author, :category_id, :book_img )
-	end
-    
-    def find_book
-    	@book=Book.find(params[:id])
-    end
+        def book_params
+            params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
+        end
+
+        def find_book
+            @book = Book.find(params[:id])
+        end
 
 end
